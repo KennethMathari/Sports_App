48400d38e7e442bd82a720bcc972cfeede8a158b
Kenneth Mathari <mathari580@gmail.com>
2022-07-26 17:40:22 +0300

    Update SportsListFragment.kt

--- app/src/main/java/com/example/android/sports/SportsListFragment.kt
+++ app/src/main/java/com/example/android/sports/SportsListFragment.kt
@@ -50,8 +50,8 @@ class SportsListFragment : Fragment() {
             // This will automatically update the dual pane content
             sportsViewModel.updateCurrentSport(it)
             // Navigate to the details screen
-            val action = SportsListFragmentDirections.actionSportsListFragmentToNewsFragment()
-            this.findNavController().navigate(action)
+            binding.slidingPaneLayout.openPane()
+
         }
         binding.recyclerView.adapter = adapter
         adapter.submitList(sportsViewModel.sportsData)
